ed584d1e8dc2caadc1d81358f19fc798763036d8
i_raj3sh <rajesh.rxca@gmail.com>
2020-08-25 23:47:03 +0530

    25/08/2020 arrays and function in pepcoding

--- temp3.cpp
+++ temp3.cpp
@@ -1,7 +1,5 @@
 #include <bits/stdc++.h>
 
-
-
 using namespace std;
 typedef long long ll;
 
@@ -11,29 +9,9 @@ ofstream zzz("output.txt");
 
 int main()
 {
-    ll num, converted_no = 0,ans ;
-    int b1, b2, d, power = 0;
-    
-    yyy >> num >> b1 >> b2; 
-    // convert no in base 10 first 
-    ll num2 = num;
-    while(num2)
-    {
-        d = num2 % 10;
-        converted_no += d*pow(b1, power);
-        num2 = num2 / 10;
-        power++;
-    }
-    
-    // then convert this into no. of another base.
-    power = 1;
-    while(converted_no)
-    {
-        d = converted_no % b2;
-        ans += power*d;
-        power *= 10;
-        converted_no /= b2;
-    }
-    
-    zzz << ans << endl;
+    int n1, n2, max;
+    n1 = 5;
+    n2  = -3;
+    vector<int>array(6);
+    zzz << array.to_string();
 }
